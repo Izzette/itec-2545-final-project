76118697bad88199af4a68f49d81028a18b159c7
Isabell Cowan <isabellcowan@gmail.com>
2017-12-03 19:02:06 -0600

    Added argument validation to CARule.

--- src/main/java/com/izzette/mctc/itec2545/final_project/CARule.java
+++ src/main/java/com/izzette/mctc/itec2545/final_project/CARule.java
@@ -3,31 +3,62 @@ package com.izzette.mctc.itec2545.final_project;
 import java.math.BigInteger;
 
 class CARule {
-	final int neighbourhoodDistance;
+	final int radius;
 	final int neighbourhoodSize;
 	final int colors;
-	final int[] ruleArray;
+	final BigInteger ruleNumber;
+
+	private final int[] ruleArray;
 
 	private static int getRuleSize(int neighbourhoodSize, int colors) {
 		int ruleSize = 1;
-		for (int i = 0; neighbourhoodSize > i; ++i)
+		for (int i = 0; neighbourhoodSize > i; ++i) {
+			if (colors > Integer.MAX_VALUE / ruleSize)
+				throw new IllegalArgumentException(
+						"Rule space is larger than the maximum integer.");
+
 			ruleSize *= colors;
+		}
 
 		return ruleSize;
 	}
 
-	CARule(int neighbourhoodDistance, int colors, BigInteger ruleNumber) {
-		this.neighbourhoodDistance = neighbourhoodDistance;
-		this.neighbourhoodSize = neighbourhoodDistance * 2 + 1;
-		this.colors = colors;
+	CARule(int radius, int colors, BigInteger ruleNumber) {
+		if (1 > radius)
+		   throw new IllegalArgumentException(
+				   "Radius must be equal to or greater than 1.");
+		if (2 > colors)
+			throw new IllegalArgumentException(
+					"Colors must be equal to or greater than 2.");
+		if (1 <= BigInteger.valueOf(0).compareTo(ruleNumber))
+			throw new IllegalArgumentException(
+					"Rule number must be postitive.");
 
-		this.ruleArray = new int[getRuleSize(this.neighbourhoodSize, colors)];
+		this.radius = radius;
+		this.neighbourhoodSize = radius * 2 + 1;
+		this.colors = colors;
+		// Save a reference to ruleNumber before changing it to produce
+		// the rule array.
+		this.ruleNumber = ruleNumber;
+
+		int ruleSize;
+		try {
+			ruleSize = getRuleSize(this.neighbourhoodSize, colors);
+		} catch (IllegalArgumentException e) {
+			// Pass down the IllegalArgumentException for rule spaces too large.
+			throw e;
+		}
+
+		this.ruleArray = new int[ruleSize];
 		BigInteger colorsBig = BigInteger.valueOf(colors);
 		for (int i = 0; this.ruleArray.length > i; ++i) {
 			BigInteger[] results = ruleNumber.divideAndRemainder(colorsBig);
 			ruleNumber = results[0];
 			this.ruleArray[i] = results[1].intValue();
 		}
+
+		if (0 != BigInteger.valueOf(0).compareTo(ruleNumber))
+			throw new IllegalArgumentException("Rule number larger than rule space.");
 	}
 
 	int applyRule(int index, int[] cells) {
@@ -37,7 +68,7 @@ class CARule {
 
 	private int computeNeighbourhood(int index, int[] cells) {
 		int neighbourhood = 0;
-		int currentIndex = wrapIndex(index - neighbourhoodDistance, cells);
+		int currentIndex = wrapIndex(index - radius, cells);
 
 		for (int i = 0; neighbourhoodSize > i; ++i) {
 			neighbourhood *= colors;
