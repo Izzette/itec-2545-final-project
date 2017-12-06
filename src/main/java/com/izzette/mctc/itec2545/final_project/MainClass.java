c37b1acf3215f44de1fd6961a821d513cb56b3cf
Isabell Cowan <isabellcowan@gmail.com>
2017-12-06 15:17:58 -0600

    Added javadoc comments to MainClass.

--- src/main/java/com/izzette/mctc/itec2545/final_project/MainClass.java
+++ src/main/java/com/izzette/mctc/itec2545/final_project/MainClass.java
@@ -2,7 +2,11 @@ package com.izzette.mctc.itec2545.final_project;
 
 import com.izzette.mctc.itec2545.final_project.CACreatorForm;
 
+/** The main class (launches CACreatorForm). */
 public class MainClass {
+	/** THe main method.
+	 * @param args The command line arguments (unused).
+	 */
 	public static void main(String[] args) {
 		CACreatorForm CACreatorForm = new CACreatorForm();
 	}
